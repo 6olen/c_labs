81934fe7742cbede3e3f0feec461eaff78c939b1
6olen <106852692+6olen@users.noreply.github.com>
2022-06-06 14:02:17 +0500

    Update lab1.c

--- lab1.c
+++ lab1.c
@@ -5,15 +5,9 @@
 int main(void) {
 int c1,c2,c3,t,min,max,mean,r;
 double e;
-printf("Enter c1: ");
-scanf("%i", &c1);
-printf("Enter c2: ");
-scanf("%i", &c2);
-printf("Enter c3: ");
-scanf("%i", &c3);
-if (c1 == c2 && c2 == c3 && c1 == c3)
-{
-printf("The program has ended! Enter different values!");
+scanf("%i%i%i", &c1, &c2, &c3);
+if (c1 == c2 && c2 == c3 && c1 == c3) {
+printf("Работа программы завершена! Введите разные значения!");
 return EXIT_SUCCESS;
 }
 
@@ -37,23 +31,23 @@ if (c3 != max) {
 c3 = 3;
 }
 
-if (c3 == 3 && c2 == 2) {
-min = 2;
-mean = 3;
+if (c3 == 3 && c3 != max) {
+ min = 2;
+ mean = 3;
 } else {
-if (c2 == max) {
-min = 3;
-mean = 1;
-} else {
-min = 1;
-mean = 2;
-}
+ if (c2 != max) {
+ min = 2;
+ mean = 1;
+ } else {
+ min = 1;
+ mean = 2;
+ }
 }
 
 printf("max=%i\n", max);
 if(max%3 == 0) {
-printf("The difference between the largest value and the sum of the numbers of the remaining values: %i\n", max-(min+mean));
+printf("Разность наибольшего числа и суммы оставшихся параметров: %i\n", max-(min+mean));
 } else {
-printf("The quotient of the sum of the parameters and the largest value: %lf\n", ((double)min+(double)mean)/(double)max);
+printf("Частное наибольшего значения и суммы параметров: %lf\n", ((double)min+(double)mean)/(double)max);
 }
 }
